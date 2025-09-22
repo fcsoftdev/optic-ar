773abf0f4d46baf03954c52fa24393ae0ab3ed03
Cristian Tosco <cristian.tosco@gmail.com>
2025-09-22 09:05:20 -0300

    Agregado de estilos a ventanas

---
+++ backend/config/apps.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from django.apps import AppConfig
+from django.apps import apps as django_apps
+
+
+class OpticArConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "config"
+
+    def ready(self):
+        from django.contrib import admin
+
+        admin.site.site_header = "Optica Punto de Vista"
+        admin.site.site_title = "Sistema de Ópticas"
+        admin.site.index_title = "Panel de administración"
+        admin.site.site_url = None
--- backend/config/settings.py
+++ backend/config/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "config.apps.OpticArConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -122,6 +123,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+
+# Carpeta donde Django juntará los archivos estáticos al hacer collectstatic
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # 👈 carpeta global de estáticos
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
